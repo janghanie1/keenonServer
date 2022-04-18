71b369fc42246f0375f80641d0f5f0ae8d0e46de
Ubuntu <janghanie1@naver.com>
2022-04-18 13:22:04 +0000

    minor change2

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-//var routes = require('./routes/index.js')
 import routes from "./routes/index.js"
 import express from 'express'
-//var express = require('express'); // 설치한 express module을 불러와서 변수(express)에 담습니다.
 
 var app = express(); //express를 실행하여 app object를 초기화 합니다.
 
