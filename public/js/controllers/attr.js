b489435023e10dedc0083d474bc950ed9b6a8023
Ubuntu <janghanie1@naver.com>
2022-04-20 14:42:52 +0000

    check sceneCode

--- public/js/controllers/attr.js
+++ public/js/controllers/attr.js
@@ -1,5 +1,17 @@
 import request from 'request'
 
+function getToken(option){
+	return new Promise(function (resolve, reject){
+		request.post(option, function(err, response, body){
+			if(!err && response.statusCode == 200){
+				resolve(body);
+                        }else{
+                                reject(err);
+                        }
+                })
+        })
+}
+
 export const getPointList = async (req, res) => {
   const clientId = process.env.clientId
   const secret = process.env.secret
@@ -27,17 +39,6 @@ export const getPointList = async (req, res) => {
 //	  json:true
   }
 
-  function getToken(option){
-        return new Promise(function (resolve, reject){
-                request.post(option, function(err, response, body){
-                        if(!err && response.statusCode == 200){
-                                resolve(body);
-                        }else{
-                                reject(err);
-                        }
-                })
-        })
-  }
   async function getTableList(){
         let credential = await getToken(tokenOption)
         tableOption.headers = {
@@ -47,3 +48,46 @@ export const getPointList = async (req, res) => {
   }
   getTableList();
 }
+
+export const getSceneList = async (req,res) => {
+  const clientId = process.env.clientId
+  const secret = process.env.secret
+  const {storeId} = req.query
+  
+  const tokenOption = {
+          uri : "https://console.peanut.keenonrobot.com/api/open/oauth/token",
+          method : "POST",
+          headers:{
+                'Content-Type':'application/x-www-form-urlencoded',
+          },
+          form:{
+                  client_id : clientId,
+                  client_secret : secret,
+                  grant_type : "client_credentials"
+          },
+          json:true
+  }
+
+  const sceneOption = {
+	  uri : "https://console.peanut.keenonrobot.com/api/open/scene/v1/info/list",
+	  method : "GET",
+	  qs:{
+		  storeId : storeId
+	  },
+	  //json:true
+  }
+  async function getSceneCode(){
+	  let credential = await getToken(tokenOption)
+	  sceneOption.headers = {
+		  Authorization : 'bearer ' + credential.access_token
+	  }
+	  request.get(sceneOption, function(err,response,body){res.send(body)})
+//	  let sceneList = await getToken(sceneOption)
+//	  console.log(sceneList)
+  }
+  getSceneCode()
+}
+
+
+
+
--- routes/attr.js
+++ routes/attr.js
@@ -1,11 +1,14 @@
 import express from "express"
-//import * as controllers from "../public/js/controllers/index.js"
 import * as attributeControllers from "../public/js/controllers/attr.js"
+//import * as controllers from "../public/js/controllers/index.js"
 
 const router = express.Router()
 
 //router.get('/table' , controllers.attributeControllers.getPointList)
+//router.get('/scene' , controllers.attributeControllers.getSceneList)
+
 router.get('/table', attributeControllers.getPointList)
+router.get('/scene', attributeControllers.getSceneList)
 
 export default router
 //module.exports = router
